2736ef295616482c09de27d872ab45f5c40ed3c0
Safiandev <safianabid78@gmail.com>
2025-11-11 16:53:03 +0000

    datatypes and varibales

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,20 @@
+// "use strict"; // treat all js code as newer version
+
+//alert(3+3) we are using node js not browser//
+
+/* 
+
+           Some variables
+string => ""
+numbers => 2 to raise power 53
+boolean => true/false
+bigint => bigger integers
+null => standalone value (object not type)
+undefiened => undefiened (type)
+symbol => unique
+let 
+const
+
+*/
+
+console.log(typeof "Safian");
---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountId = 123434
+let account_Email = "safian@google.com"
+let account_Password = "34656"
+let account_State = "Pakistan"
+
+//console.log(accountId);//
+console.table([account_Password, accountId, account_State]);
